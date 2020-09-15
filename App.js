ccfa4e4bd7b293d073628d407db6ff28a05dfe39
sgolrang <59984241+sgolrang@users.noreply.github.com>
2020-09-15 09:25:41 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LogingPageContainer from './LoginForm/LoginPageContainer';
+// import LogingPage from './LoginForm/LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+   <LogingPageContainer/>
+    </div>
+  );
+}
+
+export default App;
